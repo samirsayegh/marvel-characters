fde3b7ba2b24dd094c43d419bb58fc12da8a8658
Samir Sayegh <samirsayegh2@gmail.com>
2017-04-26 17:54:49 +0200

    Base presenters and views

---
+++ MarvelChars/app/src/main/java/com/samirsayegh/marvelchars/presenter/base/BasePresenter.java
@@ -0,0 +1,29 @@
+package com.samirsayegh.marvelchars.presenter.base;
+
+import com.samirsayegh.marvelchars.domain.services.base.BaseServiceResult;
+import com.samirsayegh.marvelchars.view.base.BaseView;
+
+/**
+ * Created by yormirsamir.sayegh on 26/04/2017.
+ */
+
+public class BasePresenter implements BaseServiceResult {
+
+    private BaseView baseView;
+
+    public BasePresenter(BaseView baseView) {
+        this.baseView = baseView;
+    }
+
+    @Override
+    public void noNetwork(String message) {
+        baseView.hideWaitingDialog();
+        baseView.showMessage(message);
+    }
+
+    @Override
+    public void error(String message) {
+        baseView.hideWaitingDialog();
+        baseView.showMessage(message);
+    }
+}
---
+++ MarvelChars/app/src/main/java/com/samirsayegh/marvelchars/view/base/BaseView.java
@@ -0,0 +1,13 @@
+package com.samirsayegh.marvelchars.view.base;
+
+/**
+ * Created by yormirsamir.sayegh on 26/04/2017.
+ */
+
+public interface BaseView {
+    void showWaitingDialog();
+
+    void hideWaitingDialog();
+
+    void showMessage(String message);
+}
